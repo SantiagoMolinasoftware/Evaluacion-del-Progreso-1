14b24af088da99c99ee96da5f28792ed4c72fedd
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2023-11-01 16:07:56 -0500

    3er commit: peticion de variables y creacion de estructuras repetitivas

--- ejercicio1.c
+++ ejercicio1.c
@@ -3,6 +3,14 @@
 int main()
 {
     int numero, suma = 0;
+    printf ("Ingresar un numero entero positivo:\n");
+    scanf ("%d",&numero);
 
+    if (numero < 0) 
+    {
+        printf("El numero ingresado no es entero positivo, vuelve a intentarlo.\n");
+    } else {
+
+    }
     return 0;
 }
